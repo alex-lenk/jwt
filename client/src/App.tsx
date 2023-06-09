07041492f8ea5c69e51535eeefcbba0add58067f
AlexLenk <alexlenk.ru@gmail.com>
2023-06-09 21:33:09 +0300

    Created basic frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useContext, useEffect, useState} from 'react';
 import './App.css';
+import LoginForm from "./components/LoginForm";
+import {Context} from "./index";
+import {IUser} from "./models/IUser";
+import UserService from "./services/UserService";
+import {observer} from "mobx-react-lite";
 
 function App() {
+  const {store} = useContext(Context);
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      store.checkAuth()
+    }
+  }, [])
+
+  async function getUsers() {
+    try {
+      const response = await UserService.fetchUsers();
+      setUsers(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  if (store.isLoading) {
+    return <div>Загрузка...</div>
+  }
+
+  if (!store.isAuth) {
+    return (
+      <div>
+        <LoginForm/>
+        <button onClick={getUsers}>Получить пользователей</button>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{store.isAuth ? `Пользователь авторизован ${store.user.email}` : 'АВТОРИЗУЙТЕСЬ'}</h1>
+      <h1>{store.user.isActivated ? 'Аккаунт подтвержден по почте' : 'ПОДТВЕРДИТЕ АККАУНТ!!!!'}</h1>
+      <button onClick={() => store.logout()}>Выйти</button>
+      <div>
+        <button onClick={getUsers}>Получить пользователей</button>
+      </div>
+      {users.map(user =>
+        <div key={user.email}>{user.email}</div>
+      )}
     </div>
   );
 }
 
-export default App;
+export default observer(App);
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, {createContext} from 'react';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Store from "./store/";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+interface State {
+  store: Store,
+}
+
+export const store = new Store();
+
+export const Context = createContext<State>({
+  store,
+})
+
+createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Context.Provider value={{store}}>
+      <App/>
+    </Context.Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
